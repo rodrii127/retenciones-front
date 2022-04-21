1dd0ab0f3e0c5b927b08c3971bcc00d568fa2d35
Rodrigo Rodriguez <rodrigo.rodriguez@valtech.com>
2022-04-21 16:51:25 -0300

    feature-login: Add sweetAlerts notification and modify type of password

---
+++ src/componentes/Alerts/SweetAlert.jsx
@@ -0,0 +1,27 @@
+import Swal from 'sweetalert2'
+
+export const loginAlert = () => {
+    Swal.fire({
+        toast: true,
+        icon: 'success',
+        title: 'Inicio de sesión exitoso',
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+}
+
+//TODO check if this is going to be reused on another component..
+export const errorAlert = (errorText) => {
+    Swal.fire({
+        title: 'Error',
+        text: errorText,
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+    })
+}
