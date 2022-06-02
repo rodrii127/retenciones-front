762c299065783244ab7a90a0ad38ac30bc28bff6
Rodrigo Rodriguez <rodrigo.rodriguez@valtech.com>
2022-06-02 11:26:52 -0300

    Change on utils

--- src/utils/UrlUtils.js
+++ src/utils/UrlUtils.js
@@ -1,6 +1,6 @@
 //TODO temporally fix until we know hot to set production enviroment on heroku server
-const enviroment = dev //prod
-const baseUrl = enviroment === dev
+const enviroment = 'dev' //prod
+const baseUrl = enviroment === 'dev'
     ? 'https://retenciones-test.herokuapp.com/v1/retenciones'
     : 'https://retentencionesnmisiones.herokuapp.com/v1/retenciones';
 
